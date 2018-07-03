5c3775d4939f41e05ba20aa4c4a33e47d73c01c1
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-07-03 11:51:15 -0500

    Added simple navigation for now

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Dashboard from './Dashboard'
 import Notes from './Notes'
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Header />
           <Route path='/' exact component={Dashboard} />
           <Route path='/notes' component={Notes} />
         </div>
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/notes">Notes</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
