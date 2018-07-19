29e439bb07f55c3695f74f2932c2e42f1754c2ff
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-07-19 00:37:52 -0500

    Added 404 handling

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Notes from './Notes';
 import Note from './Note';
 import Login from './Login';
+import NotFound from './NotFound';
 
 export default () => {
   return (
       <div>
-        <Route path='/' exact component={Dashboard} />
-        <Route path='/login' component={Login} />
-        <Route exact path='/notes' component={Notes} />
-        <Route path="/notes/:id" component={Note}/>
+        <Switch>
+          <Route path='/' exact component={Dashboard} />
+          <Route path='/login' component={Login} />
+          <Route exact path='/notes' component={Notes} />
+          <Route path="/notes/:id" component={Note}/>
+          <Route path='/404' component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
       </div>
   );
 };
