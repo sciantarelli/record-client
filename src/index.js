5351a34eff26d199b9a8b4984b3e0eb23dfbba4b
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-08-03 13:02:05 -0500

    Added bootstrap package and disabled existing styles for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// import './index.css';
 import App from './components/App';
 import { createClientStore } from './store';
 import { Provider } from 'react-redux';
