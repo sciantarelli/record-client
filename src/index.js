71c248327739a4bed5623d5bbba48db246b4d15b
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-08-06 20:41:33 -0500

    Re-enabled css index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-// import './index.css';
+import './index.css';
 import App from './components/App';
 import { createClientStore } from './store';
 import { Provider } from 'react-redux';
