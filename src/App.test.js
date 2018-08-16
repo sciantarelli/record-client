b77b591c36631799f89b0b4be130e3ec423bfe9b
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-08-16 18:53:29 -0500

    Added setupTests.js for Jest setup, and mocked localStorage and configured enzyme. Added renderAppForTesting.js which creates a wrapper for mounting the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import renderAppForTesting from './renderAppForTesting';
+
+const [,wrapper] = renderAppForTesting();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(wrapper, div);
   ReactDOM.unmountComponentAtNode(div);
 });
