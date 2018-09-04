b0ce6fe44f99ff02c539df45f3b2de70055036c1
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-09-04 14:44:04 -0500

    Added a e2e test for unauthorized action

--- src/tests/e2e/authentication.test.js
+++ src/tests/e2e/authentication.test.js
@@ -3,9 +3,11 @@ import { reseed, login, logout, currentPath, localStorageGet } from './support/h
 import { LOGIN_PATH, NOTES_PATH, AUTH_STORAGE } from '../../constants';
 import config from './support/config';
 
+
 fixture('Authentication')
     .before(reseed())
     .page(config.app_url);
+
 
 test('Login and Logout', async t => {
   await login();
@@ -26,12 +28,19 @@ test('Login and Logout', async t => {
   await t.expect(auth).typeOf('null');
 });
 
+
 test('Incorrect Login', async t => {
   await login('wrong-email@test.com', 'password');
 
   await t
     .expect(await currentPath()).eql(LOGIN_PATH)
     .expect(Selector('.error-messages').exists).ok();
+});
+
+
+test('Unauthorized Action', async t => {
+  await t.navigateTo(NOTES_PATH);
+  await t.expect(await currentPath()).eql(LOGIN_PATH);
 });
 
 
