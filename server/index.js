147071f6accc4e19843b83ee11df6dee08953443
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-12-27 00:19:40 -0600

    Updated react-scripts from 1.1.4 to 2.1.2. Also added gzip to build process and express server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import serverRenderer from './middleware/renderer';
+import expressStaticGzip from 'express-static-gzip';
 
 console.log(process.env.PORT);
 const PORT = process.env.PORT || 3000;
@@ -12,11 +13,14 @@ const router = express.Router();
 // root (/) should always serve our server rendered page
 router.use('^/$', serverRenderer);
 
+// This was the old way of serving static assets, before gzip below
+// router.use(express.static(
+//     path.resolve(__dirname, '..', 'build'),
+//     { maxAge: '30d' },
+// ));
+
 // other static resources should just be served as they are
-router.use(express.static(
-    path.resolve(__dirname, '..', 'build'),
-    { maxAge: '30d' },
-));
+router.use(expressStaticGzip("build", { maxAge: '30d' })); // TODO: Determine whether a maxAge is appropriate here at all
 
 // Everything else should be handled by React
 router.use('*', serverRenderer);
