430b8084bb05d7626d774841a4295c0442c7036f
sciantarelli <scott@mailsolo.com>
2019-09-29 21:39:52 -0400

    create Ideas list component

--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -1,9 +1,27 @@
 import React from 'react';
+
 import requireAuth from './requireAuth';
+import { AppLink } from "./Links";
+
+// TODO: crud - replace IDEAS_PATH with IDEA_PATH
+import { IDEAS_PATH, NOTES_ENDPOINT } from "../constants";
+import Fetch from "./Fetch";
 
 const Ideas = () => {
   return (
-      <div>Someday You'll Have Ideas</div>
+      <>
+        <ul>
+          <Fetch endpoint={NOTES_ENDPOINT}
+                 renderComponent={({ item }) =>
+                     <AppLink to={`${IDEAS_PATH}/${item.id}`}
+                              key={item.id}
+                              auth={true}>
+                      {item.name}
+                     </AppLink>
+                 }
+                 renderMany={true} />
+        </ul>
+      </>
   )
 };
 
